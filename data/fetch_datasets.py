df592aeb2f7e52da40e3bc66b149194c748b155a
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-09-20 14:25:35 +0200

    feat: Add 'references' config to IWAC dataset fetching script

--- data/fetch_datasets.py
+++ data/fetch_datasets.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 import os
 
 # Available configs in the IWAC dataset
-configs = ['articles', 'publications', 'documents', 'index', 'audiovisual']
+configs = ['articles', 'publications', 'documents', 'index', 'audiovisual', 'references']
 
 # Process all IWAC configs
 for config_name in configs:
